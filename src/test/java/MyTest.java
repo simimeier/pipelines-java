14206e46123b89c9c93974626bee5aaca053c63c
Simon Meier <63072220+simimeier@users.noreply.github.com>
2020-05-19 10:33:22 +0200

    Update MyTest.java
    testCases

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,6 +1,8 @@
 import com.microsoft.demo.Demo;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class MyTest {
     @Test
     public void test_method_1() {
@@ -10,5 +12,15 @@ public class MyTest {
 
     @Test
     public void test_method_2() {
+    }
+
+    @Test
+    public void test_method_3() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void test_method_4() {
+        assertTrue(true);
     }
 }
